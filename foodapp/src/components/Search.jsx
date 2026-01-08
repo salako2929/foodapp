5b0a51a7b4fe25eb520e629b61dba89a9140b0c1
salako2929 <adegokej350@gmail.com>
2026-01-08 16:09:08 +0100

    first commit

---
+++ foodapp/src/components/Search.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import styles from "./search.module.css";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "c7bbc7e4298e44c6bb2a0fcde559b387";
+export default function Search({ foodData, setFoodData }) {
+  const [query, setQuery] = useState("pizza");
+  // synthax of the useEffect hook
+  useEffect(() => {
+    async function fetchApi() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+      setFoodData(data.results);
+    }
+    fetchApi();
+  }, [query]);
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        className={styles.input}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      />
+    </div>
+  );
+}
